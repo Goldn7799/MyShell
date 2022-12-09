23bcba5ee24d935fa51eb70610047fa31f4a326a
Goldn7799 <syeifsultan@gmail.com>
2022-12-09 23:26:29 +0700

    updates and fixs

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+echo "installing requirement"
+sudo apt install nodejs npm wget git
+npm i
 echo "Downloading JS"
 git clone https://github.com/Goldn7799/MyShell.git
 cd MyShell
-echo "installing requirement"
-sudo apt install node npm -y
-npm i
 echo "Removing Template"
 rm -rf .git
 rm .gitgone
